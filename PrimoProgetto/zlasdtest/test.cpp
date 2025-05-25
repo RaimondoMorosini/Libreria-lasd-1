ef4ce9c7554daa4c7ed4ff7b8a46430ce531ee47
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-05-25 18:30:14 +0200

    inserita batteria di test del professore

--- PrimoProgetto/zlasdtest/test.cpp
+++ PrimoProgetto/zlasdtest/test.cpp
@@ -2,6 +2,9 @@
 #include "./exercise1a/test.hpp"
 #include "./exercise1b/test.hpp"
 
+#include "./exercise2a/test.hpp"
+#include "./exercise2b/test.hpp"
+
 /* ************************************************************************** */
 
 #include <iostream>
@@ -15,6 +18,7 @@ void lasdtest() {
 
   uint loctestnum, loctesterr;
   uint stestnum = 0, stesterr = 0;
+  uint ftestnum = 0, ftesterr = 0;
 
   loctestnum = 0; loctesterr = 0;
   testSimpleExercise1A(loctestnum, loctesterr);
@@ -24,7 +28,37 @@ void lasdtest() {
   testSimpleExercise1B(loctestnum, loctesterr);
   stestnum += loctestnum; stesterr += loctesterr;
 
+  loctestnum = 0; loctesterr = 0;
+  testFullExercise1A(loctestnum, loctesterr);
+  ftestnum += loctestnum; ftesterr += loctesterr;
+
+  loctestnum = 0; loctesterr = 0;
+  testFullExercise1B(loctestnum, loctesterr);
+  ftestnum += loctestnum; ftesterr += loctesterr;
+
   cout << endl << "Exercise 1 (Simple Test) (Errors/Tests: " << stesterr << "/" << stestnum << ")";
+  cout << endl << "Exercise 1 (Full Test) (Errors/Tests: " << ftesterr << "/" << ftestnum << ")" << endl;
+
+  stestnum = 0; stesterr = 0; ftestnum = 0; ftesterr = 0;
+
+  loctestnum = 0; loctesterr = 0;
+  testSimpleExercise2A(loctestnum, loctesterr);
+  stestnum += loctestnum; stesterr += loctesterr;
+
+  loctestnum = 0; loctesterr = 0;
+  testSimpleExercise2B(loctestnum, loctesterr);
+  stestnum += loctestnum; stesterr += loctesterr;
+
+  loctestnum = 0; loctesterr = 0;
+  testFullExercise2A(loctestnum, loctesterr);
+  ftestnum += loctestnum; ftesterr += loctesterr;
+
+  loctestnum = 0; loctesterr = 0;
+  testFullExercise2B(loctestnum, loctesterr);
+  ftestnum += loctestnum; ftesterr += loctesterr;
+
+  cout << endl << "Exercise 2 (Simple Test) (Errors/Tests: " << stesterr << "/" << stestnum << ")";
+  cout << endl << "Exercise 2 (Full Test) (Errors/Tests: " << ftesterr << "/" << ftestnum << ")" << endl;
 
   cout << endl << "Goodbye!" << endl;
 }
