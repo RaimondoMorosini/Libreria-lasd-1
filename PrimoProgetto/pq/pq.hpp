d90416535e9fd8706fbd4eddb2038e87fed295c5
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-05-30 19:30:28 +0200

    coretto signature di insert

--- PrimoProgetto/pq/pq.hpp
+++ PrimoProgetto/pq/pq.hpp
@@ -29,8 +29,8 @@ public:
   virtual void RemoveTip() = 0; // Remove the tip of the priority queue (must throw std::length_error when empty)
   virtual Data TipNRemove() = 0; // Get the tip of the priority queue and remove it (must throw std::length_error when empty)
 
-  virtual bool Insert(const Data&) = 0; // Insert a value into the priority queue (copy of the value)
-  virtual bool Insert(Data&&) = 0; // Insert a value into the priority queue (move of the value)
+  virtual void Insert(const Data&) = 0; // Insert a value into the priority queue (copy of the value)
+  virtual void Insert(Data&&) = 0; // Insert a value into the priority queue (move of the value)
 
 
   virtual void Change(const ulong, const Data&) = 0; // Change the tip of the priority queue (copy of the value)
