4832f3c760230138a28689a70cd50ee606713533
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-05-30 12:28:00 +0200

    Update pqheap.hpp

--- PrimoProgetto/pq/heap/pqheap.hpp
+++ PrimoProgetto/pq/heap/pqheap.hpp
@@ -9,71 +9,78 @@
 
 /* ************************************************************************** */
 
-namespace lasd {
+namespace lasd
+{
 
-/* ************************************************************************** */
+  /* ************************************************************************** */
 
-template <typename Data>
-class PQHeap : virtual public PQ<Data>,
-                virtual protected HeapVec<Data> { 
-protected:
+  template <typename Data>
+  class PQHeap : virtual public PQ<Data>,
+                 virtual protected HeapVec<Data>
+  {
+  protected:
+    using Container::size;
+    using Vector<Data>::Elements;
+    using Vector<Data>::Resize;
+    using HeapVec<Data>::HeapifyDown;
+    using HeapVec<Data>::HeapifyUp;
 
-  using Container::size;
-  using Vector<Data>::Elements;
-  using Vector<Data>::Resize;
-  using HeapVec<Data>::HeapifyDown;
-  using HeapVec<Data>::HeapifyUp;
+    ulong heapSize = 0; // Size of the heap (number of elements in the heap)
 
-public:
+  public:
+    PQHeap() = default; // Default constructor
 
-  PQHeap() = default; // Default constructor
-  
+    /* ************************************************************************ */
 
-  /* ************************************************************************ */
+    // Specific constructors
 
-  // Specific constructors
-  
-  // PQHeap(argument) specifiers; // A priority queue obtained from a TraversableContainer
-  PQHeap(const TraversableContainer<Data>& con) : HeapVec<Data>(con) {}  
-  // PQHeap(argument) specifiers; // A priority queue obtained from a MappableContainer
-  PQHeap(MappableContainer<Data>&& con) : HeapVec<Data>(std::move(con)) {}
-  /* ************************************************************************ */
+    // PQHeap(argument) specifiers; // A priority queue obtained from a TraversableContainer
+    PQHeap(const TraversableContainer<Data> &con) : HeapVec<Data>(con) {
+      heapSize = con.Size(); // Initialize heap size from the container size
+    }
+    // PQHeap(argument) specifiers; // A priority queue obtained from a MappableContainer
+    PQHeap(MappableContainer<Data> &&con) : HeapVec<Data>(std::move(con)) {
+      heapSize = con.Size(); // Initialize heap size from the container size
+    }
+    /* ************************************************************************ */
 
-  // Copy constructor
-  PQHeap(const PQHeap<Data>& other) : HeapVec<Data>(other) {} 
-  
-  // Move constructor
-  PQHeap(PQHeap<Data>&& other) noexcept : HeapVec<Data>(std::move(other)) {} 
-  /* ************************************************************************ */
+    // Copy constructor
+    PQHeap(const PQHeap<Data> &other) : HeapVec<Data>(other) {
+      heapSize = other.heapSize; // Copy the heap size
+     }
 
-  ~PQHeap()= default; // Destructor
+    // Move constructor
+    PQHeap(PQHeap<Data> &&other) noexcept : HeapVec<Data>(std::move(other)) {
+      std::swap(heapSize, other.heapSize); // Move the heap size
+    }
+    /* ************************************************************************ */
 
-  /* ************************************************************************ */
+    ~PQHeap() = default; // Destructor
 
-  // Copy assignment
-PQHeap<Data>& operator=(const PQHeap<Data>& );
-  // Move assignment
-PQHeap<Data>& operator=(PQHeap<Data>&& ) noexcept ;
-  /* ************************************************************************ */
+    /* ************************************************************************ */
 
-  // Specific member functions (inherited from PQ)
+    // Copy assignment
+    PQHeap<Data> &operator=(const PQHeap<Data> &);
+    // Move assignment
+    PQHeap<Data> &operator=(PQHeap<Data> &&) noexcept;
+    /* ************************************************************************ */
 
-  const Data& Tip() const override; // Override PQ member (must throw std::length_error when empty)
-  void RemoveTip() override; // Override PQ member (must throw std::length_error when empty)                
-  Data TipNRemove() override; // Override PQ member (must throw std::length_error when empty)
-  
- void Insert(const Data& dat) override; // Override PQ member (Copy of the value) 
-  void Insert(Data&& dat) override; // Override PQ member (Move of the value)
-  
-  void Change(const ulong index, const Data& dat) override; // Override PQ member (Copy of the value)
-  void Change(const ulong index, Data&& dat) override; // Override PQ member (Move of the value)
+    // Specific member functions (inherited from PQ)
 
-protected:
+    const Data &Tip() const override; // Override PQ member (must throw std::length_error when empty)
+    void RemoveTip() override;        // Override PQ member (must throw std::length_error when empty)
+    Data TipNRemove() override;       // Override PQ member (must throw std::length_error when empty)
 
+    void Insert(const Data &dat) override; // Override PQ member (Copy of the value)
+    void Insert(Data &&dat) override;      // Override PQ member (Move of the value)
 
-};
+    void Change(const ulong index, const Data &dat) override; // Override PQ member (Copy of the value)
+    void Change(const ulong index, Data &&dat) override;      // Override PQ member (Move of the value)
 
-/* ************************************************************************** */
+  protected:
+  };
+
+  /* ************************************************************************** */
 
 }
 
